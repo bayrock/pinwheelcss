419e1263cd7ff1ad05c8938c2585946a79953917
Bailey Chestnut <bayrock@users.noreply.github.com>
2023-01-23 01:44:12 -0500

    Update build config from Object to Map

--- build.js
+++ build.js
@@ -3,17 +3,16 @@ const postcss = require('postcss')
 const { plugins } = require('./postcss.config')
 const schema = require('./package.json')
 
-const config = Object.freeze({
-    source: 'src/css/pinwheel.pcss',
-    directory: `dist/@${schema.version}`,
-    filename: `pinwheel.min.css`
-})
+const config = new Map()
+    .set('source', 'src/css/pinwheel.pcss')
+    .set('directory', `dist/@${schema.version}`)
+    .set('filename', `pinwheel.min.css`)
 
-const path = `${config.directory}/${config.filename}`
+const path = `${config.get('directory')}/${config.get('filename')}`
 const tag = `[${schema.name}@${schema.version}]`
 
-fs.readFile(config.source)
-    .then(precompiled => postcss(plugins).process(precompiled, { from: config.source, to: path }))
-    .then(processed => fs.mkdir(config.directory, { recursive: true }).then(() => processed.css))
+fs.readFile(config.get('source'))
+    .then(precompiled => postcss(plugins).process(precompiled, { from: config.get('source'), to: path }))
+    .then(processed => fs.mkdir(config.get('directory'), { recursive: true }).then(() => processed.css))
     .then(compiled => fs.writeFile(path, compiled)).then(() => console.log(`${tag} Build successful!`))
     .catch(error => console.log(`${tag} Build error:\n${error.message}`))
